1b22d4820fa7a7c49e36b6682bb812a7199df123
ElrohirGT <elrohirgt@gmail.com>
2026-03-11 16:38:15 -0600

    fix: bear command

--- scripts/build-scripts/qtz-bear.sh
+++ scripts/build-scripts/qtz-bear.sh
@@ -8,5 +8,12 @@ source "$DIR_NAME"/../quit-if-environment-is-wrong.sh
 bear -o $RODOS_SRC/bare-metal/stm32h7/cm4/compile_commands.json -- qtz-lib.sh cm4
 bear -o $RODOS_SRC/bare-metal/stm32h7/cm7/compile_commands.json -- qtz-lib.sh cm7
 
-bear -o $QTZ_CM4/compile_commands.json -- qtz-elf.sh cm4 $QTZ_CM4/main.cpp
-bear -o $QTZ_CM7/compile_commands.json -- qtz-elf.sh cm7 $QTZ_CM7/main.cpp
+PREV=$PWD
+cd $QTZ_CM4
+bear -o compile_commands.json -- qtz-elf.sh cm4 main.cpp
+cd "$PREV"
+
+PREV=$PWD
+cd $QTZ_CM7
+bear -o compile_commands.json -- qtz-elf.sh cm7 main.cpp
+cd "$PREV"
